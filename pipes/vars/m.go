2218d66c174354c936ec2cf0b71ea4521c949128
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2021-08-31 19:55:47 +0200

    Delete pipes directory

--- pipes/vars/m.go
+++
@@ -1,6 +0,0 @@
-package vars
-
-import "image/color"
-
-var C = []*color.RGBA{{R: 255, G: 0, B: 0, A: 10}, {R: 255, G: 255, B: 0, A: 10},
-{R: 0, G: 0, B: 255, A: 10}, {R: 0, G: 255, B: 255, A: 10}}
