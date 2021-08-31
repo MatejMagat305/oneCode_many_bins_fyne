79c79d3966853b621099f347df3942485bbb4fab
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2021-08-31 17:01:23 +0200

    Delete comand.sh

--- simple_snake/comand.sh
+++
@@ -1,3 +0,0 @@
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/tools
-fyneLdflags package -os android/arm64 -appID com.cocoacasts.scribbles -name myAppGoLdflags -icon snake.png
